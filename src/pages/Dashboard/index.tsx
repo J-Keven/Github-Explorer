964fc2c1ac8397023db305f3e6a09d56145a5571
J-keven <jhonasnunes425@gmail.com>
2020-08-31 19:19:05 -0300

    [feat]: add adding folders sctructure

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h2>Dashboard page</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
